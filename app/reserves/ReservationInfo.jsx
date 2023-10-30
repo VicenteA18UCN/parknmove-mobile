55722088d65f7c623b2c6fa4e86c034c952c8ba9
Joaquin Pinto <joaquin-pinto@alumnos.ucn.cl>
2023-10-30 00:56:55 -0300

    Se agrega qr a la reserva

--- app/reserves/ReservationInfo.jsx
+++ app/reserves/ReservationInfo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import QRCode from 'react-native-qrcode-svg';
 
 const ReservationInfo = ({ route }) => {
   // Obten los datos de la reserva de las props
@@ -7,9 +8,12 @@ const ReservationInfo = ({ route }) => {
 
   // Formatear la hora de entrada
   const entryTime = new Date(reservationData.response.entry_time).toLocaleString();
+  // Combina toda la información en una cadena que se incluirá en el código QR
+  const qrData = `Usuario: ${reservationData.userName}\nEstacionamiento: ${reservationData.parkingName}\nHora de entrada: ${entryTime}\nCosto por hora: $${reservationData.response.extra_fee}`;
 
   return (
     <View style={styles.container}>
+      <View style={styles.space} />
       <Text style={styles.title}>Detalles de la Reserva</Text>
       <View style={styles.infoContainer}>
         <Text style={styles.label}>Usuario:</Text>
@@ -27,6 +31,10 @@ const ReservationInfo = ({ route }) => {
         <Text style={styles.label}>Costo por hora:</Text>
         <Text style={styles.value}>${reservationData.response.extra_fee}</Text>
       </View>
+      <View style={styles.space} />
+      <View style={styles.qrContainer}>
+        <QRCode value={qrData} size={200} />
+      </View>
     </View>
   );
 };
@@ -41,10 +49,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 20,
   },
+  qrContainer: {
+    alignItems: 'center', // Centra el código QR
+  },
   infoContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     marginBottom: 10,
+  },
+  space: {
+    marginBottom: 30, // Agrega espacio entre elementos
   },
   label: {
     fontSize: 16,
