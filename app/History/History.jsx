42d904632633e4b63d7dca2d8b94ef0011f915a1
ManVeraM <84684731+ManVeraM@users.noreply.github.com>
2023-11-11 04:28:16 -0300

    Added: first styled view

--- app/History/History.jsx
+++ app/History/History.jsx
@@ -1,6 +1,27 @@
 
 import React, { useState, useEffect } from 'react';
 import agent from "../../api/agent";
+import {
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle as Title,
+  SubTitle,
+  StyledFormArea,
+  StyledTextInput,
+  StyledButton,
+  StyledInputLabel,
+  LeftIcon,
+  RightIcon,
+  ButtonText,
+  Colors,
+  Line,
+  MsgBox,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+} from "../../components/styles";
 
 
 
@@ -59,19 +80,31 @@ const ReservationHistory = ( ) =>{
 
 useEffect(() => { fetchHistory(); }, []);
 
+  const buttonStyle = {
+    marginRight: '10px', // Ajusta el valor de margen según tus preferencias
+  };
+
 
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Historial de Reservas</h1>
-      <div style={{ textAlign: 'center' }}>
-          <button onClick={() => changeInterval('todos')}>Todos</button>
-          <button onClick={() => changeInterval('ultimoMes')}>Último Mes</button>
-          <button onClick={() => changeInterval('ultimaSemana')}>Última Semana</button>
-          <button onClick={() => changeInterval('ultimoAnio')}>Último Año</button>
+        <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+          <StyledButton onPress={() => changeInterval('todos')}style={buttonStyle}>
+            <ButtonText>Todos</ButtonText>
+          </StyledButton>
+          <StyledButton onPress={() => changeInterval('ultimoMes')}style={buttonStyle}>
+            <ButtonText>Ultimo mes</ButtonText>
+          </StyledButton>
+          <StyledButton onPress={() => changeInterval('ultimaSemana')}style={buttonStyle}>
+            <ButtonText>Ultima semana</ButtonText>
+          </StyledButton>
+          <StyledButton onPress={() => changeInterval('ultimoAnio')}style={buttonStyle}>
+            <ButtonText>Ultimo año</ButtonText>
+          </StyledButton>
         </div>
         {filteredReservations.length > 0 ? (
-        <table style={{ margin: '0 auto' }}>
+        <table style={{ margin: '0 auto', borderCollapse: 'collapse', width: '80%' }}>
           <thead>
             <tr>
               <th>Lugar</th>
@@ -81,14 +114,14 @@ useEffect(() => { fetchHistory(); }, []);
             </tr>
           </thead>
           <tbody>
-          {filteredReservations.map((Reservation) => (
-            <tr key={Reservation.id}>
-              <td>{Reservation.direccion}</td>
-              <td>{new Date(Reservation.entry_time).toLocaleDateString()}</td>
-              <td>{new Date(Reservation.entry_time).toLocaleTimeString()}</td>
-              <td>{Reservation.extra_fee}</td>
-            </tr>
-          ))}
+            {filteredReservations.map((Reservation, index) => (
+              <tr key={Reservation.id} style={{ backgroundColor: index % 2 === 0 ? '#f2f2f2' : 'white' }}>
+              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{Reservation.direccion}</td>
+              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{new Date(Reservation.entry_time).toLocaleDateString()}</td>
+              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{new Date(Reservation.entry_time).toLocaleTimeString()}</td>
+              <td style={{ border: '1px solid #ccc', padding: '8px', textAlign: 'center' }}>{Reservation.extra_fee}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       ) : (
