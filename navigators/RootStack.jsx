30e080084a727bbb6543b76b83d65e128c718d00
ManVeraM <84684731+ManVeraM@users.noreply.github.com>
2023-11-12 16:59:47 -0300

    test

--- navigators/RootStack.jsx
+++ navigators/RootStack.jsx
@@ -27,7 +27,7 @@ const RootStack = () => {
           headerTitle: "",
           headerLeftContainerStyle: { paddingLeft: 20 },
         }}
-        initialRouteName="Reserva"
+        initialRouteName="Login"
       >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
